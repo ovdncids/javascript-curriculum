d76c353847781862fa6f1eaec46fe99f2f058171
ovdncids <ovdncids@nate.com>
2021-03-16 13:57:14 +0900

    Create form.js

---
+++ docs/form/form.js
@@ -0,0 +1,48 @@
+const membersGet = sessionStorage.getItem('members');
+const membersLogical = membersGet || '[]';
+const members = JSON.parse(membersLogical);
+
+const membersCreate = function(member) {
+  members.push(member);
+  membersSet();
+  const read = membersRead();
+  return read;
+};
+
+const membersRead = function() {
+  // window.location.reload();
+  return members;
+};
+
+const membersDelete = function(index) {
+  members.splice(index, 1);
+  membersSet();
+  const read = membersRead();
+  return read;
+};
+
+const membersUpdate = function(index, name) {
+  members[index] = name;
+  membersSet();
+  const read = membersRead();
+  return read;
+};
+
+const membersSet = function() {
+  const membersSet = JSON.stringify(members);
+  sessionStorage.setItem('members', membersSet);
+};
+
+const membersSubmit = function(form) {
+  const textNameObject = form['text-name'];
+  try {
+    const evalReturn = eval(textNameObject.value);
+  } catch(error) {
+    console.error(error);
+    return false;
+  }
+}
+
+for (let index in members) {
+  document.write(members[index] + ' ');
+}
