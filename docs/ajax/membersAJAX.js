9c0d631a9eb70411633879366376986859152aa6
ovdncids <ovdncids@nate.com>
2021-03-20 08:35:55 +0900

    Create membersAJAX.js

---
+++ docs/ajax/membersAJAX.js
@@ -0,0 +1,121 @@
+const membersCreate = function(form) {
+  const memberNameObject = form['member-name'];
+  const memberAgeObject = form['member-age'];
+  const member = {
+    name: memberNameObject.value,
+    age: memberAgeObject.value
+  };
+  const successFunction = function() {
+    memberNameObject.value = '';
+    memberAgeObject.value = '';
+    membersRead();
+  }
+  const xhrObject = new XMLHttpRequest();
+  xhrObject.onreadystatechange = function() {
+    if (xhrObject.readyState !== 4) return;
+    if (xhrObject.status === 200) {
+      successFunction();
+    } else {
+      const error = {
+        status: xhrObject.status,
+        statusText: xhrObject.statusText,
+        responseText: xhrObject.responseText
+      }
+      console.error(error);
+    }
+  };
+  xhrObject.open('POST', 'http://localhost:3100/api/v1/members');
+  xhrObject.setRequestHeader('Content-Type', 'application/json');
+  xhrObject.send(JSON.stringify(member));
+};
+
+const membersRead = function() {
+  const successFunction = function(xhrObject) {
+    const membersLogical = JSON.parse(xhrObject.responseText);
+    const members = membersLogical.members;
+    const tagDivParent = document.getElementById('tag-div-parent');
+    const tagDivChild = document.getElementById('tag-div-child');
+    tagDivParent.innerHTML = '';
+    for (let index in members) {
+      const newDivChild = tagDivChild.cloneNode(true);
+      tagDivParent.appendChild(newDivChild);
+      const membersNameObject = document.getElementsByName('members-name')[index];
+      const membersAgeObject = document.getElementsByName('members-age')[index];
+      const membersUpdateObject = document.getElementsByName('members-update')[index];
+      const membersDeleteObject = document.getElementsByName('members-delete')[index];
+      membersNameObject.value = members[index].name;
+      membersAgeObject.value = members[index].age;
+      membersUpdateObject.index = index;
+      membersDeleteObject.index = index;
+    }
+    console.log('Readed', members);
+  };
+  const xhrObject = new XMLHttpRequest();
+  xhrObject.onreadystatechange = function () {
+    if (xhrObject.readyState !== 4) return;
+    if (xhrObject.status === 200) {
+      successFunction(xhrObject);
+    } else {
+      const error = {
+        status: xhrObject.status,
+        statusText: xhrObject.statusText,
+        responseText: xhrObject.responseText
+      }
+      console.error(error);
+    }
+  };
+  xhrObject.open('GET', 'http://localhost:3100/api/v1/members');
+  xhrObject.setRequestHeader('Content-Type', 'application/json');
+  xhrObject.send();
+};
+
+const membersDelete = function(index) {
+  const xhrObject = new XMLHttpRequest();
+  xhrObject.onreadystatechange = function () {
+    if (xhrObject.readyState !== 4) return;
+    if (xhrObject.status === 200) {
+      membersRead();
+    } else {
+      const error = {
+        status: xhrObject.status,
+        statusText: xhrObject.statusText,
+        responseText: xhrObject.responseText
+      }
+      console.error(error);
+    }
+  };
+  xhrObject.open('DELETE', 'http://localhost:3100/api/v1/members/' + index);
+  xhrObject.setRequestHeader('Content-Type', 'application/json');
+  xhrObject.send();
+};
+
+const membersUpdate = function(index) {
+  const name = document.getElementsByName('members-name')[index].value;
+  const age = document.getElementsByName('members-age')[index].value;
+  const memberUpdate = {
+    index: index,
+    member: {
+      name: name,
+      age: age
+    }
+  };
+  const xhrObject = new XMLHttpRequest();
+  xhrObject.onreadystatechange = function () {
+    if (xhrObject.readyState !== 4) return;
+    if (xhrObject.status === 200) {
+      membersRead();
+    } else {
+      const error = {
+        status: xhrObject.status,
+        statusText: xhrObject.statusText,
+        responseText: xhrObject.responseText
+      }
+      console.error(error);
+    }
+  };
+  xhrObject.open('PATCH', 'http://localhost:3100/api/v1/members');
+  xhrObject.setRequestHeader('Content-Type', 'application/json');
+  xhrObject.send(JSON.stringify(memberUpdate));
+};
+
+membersRead();
